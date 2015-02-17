7617320ff4d0045b72cd23f6153daf66a08c6630
David Greenspan <dgreenspan@alum.mit.edu>
2015-02-16 16:03:39 -0800

    Display some rectangles, use less

--- rectangles.js
+++ rectangles.js
@@ -1,17 +1,35 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Template.dashboard.helpers({
+    layout: function () {
+      var board = { columns: [[{type: "photo"}, {type: "text"}],
+                              [{type: "text"}, {type: "photo"}]] };
+      var layout = { columns: _.map(board.columns, function (rects, colNum) {
+        var colWidth = 100. / board.columns.length;
+        var numFlex = 0;
+        var amountFlex = 100.;
+        _.each(rects, function (rect) {
+          if (rect.type === 'text') {
+            numFlex++;
+          } else if (rect.type === 'photo') {
+            amountFlex -= colWidth * 0.75;
+          }
+        });
+        var totalHeight = 0;
+        return _.map(rects, function (rect) {
+          var height = (rect.type === 'text' ? amountFlex/numFlex :
+                        colWidth * 0.75);
+          var top = totalHeight;
+          totalHeight += height;
+          return { type: rect.type,
+                   width: colWidth,
+                   height: height,
+                   left: colNum*colWidth,
+                   top: top
+                 };
+        });
+      }) };
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      return layout;
     }
   });
 }
