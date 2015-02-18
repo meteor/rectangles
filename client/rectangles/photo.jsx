3408215bb3546a06d3fe83c3572c26ca3e2d8a27
Avital Oliver <avital@thewe.net>
2015-02-18 15:12:21 -0800

    photo urls in database

--- client/rectangles/photo.jsx
+++ client/rectangles/photo.jsx
@@ -2,12 +2,9 @@ PhotoRectangle = React.createClass({
   render: function () {
     return <div className="cycle-slideshow"
       data-cycle-fx="flipHorz"
-      data-cycle-timeout="8000"
-      >
-      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p1.jpg" />
-      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p2.jpg" />
-      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p3.jpg" />
-      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p4.jpg" />
+      data-cycle-timeout={_.random(5000, 10000)}>
+      {_.map(this.props.urls, (url) =>
+        <img style={{width: "100%", height: "100%"}} src={url} />)}
     </div>;
   },
 
