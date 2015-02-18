2808c8ecb8337dee886eab93e27cd88dc7d105ff
Avital Oliver <avital@thewe.net>
2015-02-18 15:41:59 -0800

    WIP: choose photos from dropbox

--- client/rectangles/photo.jsx
+++ client/rectangles/photo.jsx
@@ -1,14 +1,33 @@
 PhotoRectangle = React.createClass({
+  getInitialState: function () {
+    return {
+      dropboxVisible: false
+    }
+  },
   render: function () {
-    return <div className="cycle-slideshow"
-      data-cycle-fx="flipHorz"
-      data-cycle-timeout={_.random(5000, 10000)}>
-      {_.map(this.props.urls, (url) =>
-        <img style={{width: "100%", height: "100%"}} src={url} />)}
+    return <div onMouseEnter={this.setState.bind(this, {dropboxVisible: true}, null)} onMouseLeave={this.setState.bind(this, {dropboxVisible: false}, null)}>
+      <div className="dropbox-container" ref="dropbox-container" style={{visibility: this.state.dropboxVisible ? "" : "hidden"}}></div>
+      <div className="cycle-slideshow" ref="cycle-slideshow"
+        data-cycle-fx="flipHorz"
+        data-cycle-timeout={_.random(5000, 10000)}>
+        {_.map(this.props.urls, (url) =>
+          <img style={{width: "100%", height: "100%"}} src={url} />)}
+      </div>
     </div>;
   },
 
   componentDidMount: function () {
-    $(this.getDOMNode()).cycle();
+    $(this.refs["cycle-slideshow"].getDOMNode()).cycle();
+
+    var button = Dropbox.createChooseButton({
+      success: function (files) {
+        console.log(files);
+      },
+
+      linkType: "direct",
+      multiselect: true,
+      extensions: [".png", ".jpg", ".jpeg", ".gif"]
+    });
+    this.refs["dropbox-container"].getDOMNode().appendChild(button);
   }
 });
