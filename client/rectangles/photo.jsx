1088af7c5980ce564f8f8fa9f9541d8b5cd79a06
Avital Oliver <avital@thewe.net>
2015-02-18 14:17:28 -0800

    WIP: hard-coded photos

--- client/rectangles/photo.jsx
+++ client/rectangles/photo.jsx
@@ -1,5 +1,17 @@
 PhotoRectangle = React.createClass({
   render: function () {
-    return <span>photo</span>;
+    return <div className="cycle-slideshow"
+      data-cycle-fx="flipHorz"
+      data-cycle-timeout="8000"
+      >
+      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p1.jpg" />
+      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p2.jpg" />
+      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p3.jpg" />
+      <img style={{width: "100%", height: "100%"}} src="http://malsup.github.io/images/p4.jpg" />
+    </div>;
+  },
+
+  componentDidMount: function () {
+    $(this.getDOMNode()).cycle();
   }
 });
